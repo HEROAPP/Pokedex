1cca0ad282e50351c41276503480691af576a3bd
YOUNIS <younis@YOUNISs-Mac.local>
2016-03-30 20:32:56 +0300

    added musicBtn

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 //UICollectionViewDataSource: we saying this view we also gonna manage the data that shows up inside our CollectionView
 //UICollectionViewDelegateFlowLayout : basically this is the deleget has deleget and func to selt layout on the CollectionView like sizing and spacing , ...
 class ViewController: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var collection: UICollectionView!
+    
+    @IBOutlet weak var searchBat: UISearchBar!
+    
+    var pokemon = [Pokemon]()
+    
+    var musicPlayer : AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +28,48 @@ class ViewController: UIViewController , UICollectionViewDelegate , UICollection
         collection.delegate = self
         collection.dataSource = self
         
+        
+        
+        initAudio()
+        pasrePokemonCSV()
     }
 
+    //when the app first load we want to crap all the data from it ,creat punch of pokemon, and load their name into the screen 
+    
+    func pasrePokemonCSV () {
+        
+        //crap the file and then run the pasre on it
+        
+        let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
+        //! we know the file is there 
+        
+        //we run the pasre which can throw  error 
+        
+        do {
+            //we crap the path of the csv file,,parse it ti csv
+            let csv = try CSV(contentsOfURL: path)
+            
+            //crap the row out of csv file
+            let rows = csv.rows
+            
+            for row in rows {
+                //each row is dictionary
+                
+                let pokeId = Int(row["id"]!)!
+                let name = row["identifier"]!
+                
+                let poke = Pokemon(name: name, pokedex: pokeId)
+                
+                pokemon.append(poke)
+                
+            }
+            
+        }catch let err as NSError{
+            //if there is a problem
+            print(err.debugDescription)
+        }
+        
+    }
     
     
     
@@ -32,8 +79,8 @@ class ViewController: UIViewController , UICollectionViewDelegate , UICollection
         //indexPath is the current index of the row
         if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
             
-            let pokemon = Pokemon(name: "test", pokedex: indexPath.row)
-            cell.configureCell(pokemon)
+            let poke = pokemon[indexPath.row]
+            cell.configureCell(poke)
             
             return cell
         }else {
@@ -52,7 +99,7 @@ class ViewController: UIViewController , UICollectionViewDelegate , UICollection
 
     //we need to know how many item in each section
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 718
+        return pokemon.count
     }
     
     
@@ -66,6 +113,41 @@ class ViewController: UIViewController , UICollectionViewDelegate , UICollection
         
         return CGSize(width: 105, height: 105)
         
+    }
+    
+    
+    func initAudio(){
+       
+        let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")!
+        
+        do {
+            
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path)!)
+            musicPlayer.prepareToPlay()
+            
+            //we want infinite loop
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        }catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+        
+    }
+    
+    
+    
+    @IBAction func musicBtnPressed(sender: UIButton!) {
+        
+        //if playing
+        if musicPlayer.playing {
+            musicPlayer.stop()
+            sender.alpha = 0.3
+        }else{
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
     }
 }
 
